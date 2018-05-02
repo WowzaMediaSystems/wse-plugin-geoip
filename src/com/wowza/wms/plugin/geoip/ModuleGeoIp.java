c70dc47c26af9369181f8f25a180462f28fe0867
Roger Littin <roger@wowza.com>
2018-05-03 10:55:45 +1200

    Initial commit

---
+++ src/com/wowza/wms/plugin/geoip/ModuleGeoIp.java
@@ -0,0 +1,236 @@
+/*
+ * This code and all components (c) Copyright 2006 - 2018, Wowza Media Systems, LLC. All rights reserved.
+ * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
+ */
+package com.wowza.wms.plugin.geoip;
+
+import com.wowza.wms.application.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CountryResponse;
+import com.maxmind.geoip2.record.Country;
+import com.wowza.util.StringUtils;
+import com.wowza.wms.amf.*;
+import com.wowza.wms.client.*;
+import com.wowza.wms.module.*;
+import com.wowza.wms.request.*;
+import com.wowza.wms.util.SecurityUtils;
+import com.wowza.wms.rtp.model.*;
+import com.wowza.wms.rtsp.RTSPRequestMessage;
+import com.wowza.wms.rtsp.RTSPResponseMessages;
+import com.wowza.wms.server.Server;
+import com.wowza.wms.httpstreamer.model.*;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.logging.WMSLoggerIDs;
+
+public class ModuleGeoIp extends ModuleBase
+{
+	class RTSPActionNotifyListener extends  RTSPActionNotifyBase
+	{
+		@Override
+		public void onDescribe(RTPSession rtpSession, RTSPRequestMessage req, RTSPResponseMessages resp)
+		{
+			try
+			{
+				if(debugLog)
+					logger.info(String.format("%s::RTSPActionNotifyListener.onDescribe [%s] sessionIp: %s", CLASSNAME, appInstance.getContextStr(), rtpSession.getIp()), category, event);
+				if(SecurityUtils.isInIPList(rtpSession.getIp(), allowedIps))
+					return;
+				if(checkAddress(rtpSession.getIp()))
+					return;
+				
+				rtpSession.rejectSession();
+			}
+			catch (Exception e)
+			{
+				logger.error(CLASSNAME + "::RTSPActionNotifyListener.onDescribe [" + appInstance.getContextStr()  +"] error occured.", e);
+			}
+		}
+
+		@Override
+		public void onAnnounce(RTPSession rtpSession, RTSPRequestMessage req, RTSPResponseMessages resp)
+		{
+			try
+			{
+				if(debugLog)
+					logger.info(String.format("%s::RTSPActionNotifyListener.onAnnounce [%s] sessionIp: %s, userAgent: %s", CLASSNAME, appInstance.getContextStr(), rtpSession.getIp(), rtpSession.getUserAgent()), category, event);
+				if(SecurityUtils.isValidFlashVersion(rtpSession.getUserAgent(), allowedEncoders))
+					return;
+				if(SecurityUtils.isInIPList(rtpSession.getIp(), allowedIps))
+					return;
+				if(checkAddress(rtpSession.getIp()))
+					return;
+				
+				rtpSession.rejectSession();
+			}
+			catch (Exception e)
+			{
+				logger.error(CLASSNAME + "::RTSPActionNotifyListener.onAnnounce [" + appInstance.getContextStr()  +"] error occured.", e);
+			}
+		}
+	}
+	
+	public static final String CLASSNAME = "ModuleGeoIP";
+	
+	private WMSLogger logger;
+	private IApplicationInstance appInstance;
+	private ServerListenerGeoIp geoIpReader = (ServerListenerGeoIp)Server.getInstance().getProperties().get(ServerListenerGeoIp.PROP_GEOIP_READER);
+	private RTSPActionNotifyListener rtspListener = new RTSPActionNotifyListener();
+	private String allowedEncodersStr = SecurityUtils.DEFAULT_AUTHFLASHVERSIONS;
+	private List<String> allowedEncoders = new ArrayList<String>();
+	private String allowedIpsStr = "";
+	private List<String> allowedIps;
+	private String countriesStr = "*";
+	private List<String> countries;
+	private boolean matchAllow = true;
+	private boolean noMatchAllow = false;
+	private boolean debugLog = false;
+	private String category = WMSLoggerIDs.CAT_application;
+	private String event = WMSLoggerIDs.EVT_comment;
+
+	public void onAppStart(IApplicationInstance appInstance)
+	{
+		logger = WMSLoggerFactory.getLoggerObj(appInstance);
+		this.appInstance = appInstance;
+		// use ModuleCoreSecurity property if set. 
+		allowedEncodersStr = appInstance.getProperties().getPropertyStr("securityPublishValidEncoders", allowedEncodersStr);
+		allowedEncodersStr = appInstance.getProperties().getPropertyStr("geoIpAllowedEncoders", allowedEncodersStr);
+		allowedEncoders = SecurityUtils.parseValidFlashStrings(allowedEncodersStr);
+		allowedIpsStr = appInstance.getProperties().getPropertyStr("geoIpAllowedIps", allowedIpsStr);
+		allowedIps = splitPropertyIntoList(allowedIpsStr, true);
+		countriesStr = appInstance.getProperties().getPropertyStr("geoIpCountries", countriesStr);
+		countries = splitPropertyIntoList(countriesStr, false);
+		matchAllow = appInstance.getProperties().getPropertyBoolean("geoIpMatchAllow", matchAllow);
+		noMatchAllow = appInstance.getProperties().getPropertyBoolean("geoIpNoMatchAllow", noMatchAllow);
+		debugLog = appInstance.getProperties().getPropertyBoolean("geoIpDebugLog", debugLog);
+		if(logger.isDebugEnabled())
+			debugLog = true;
+		
+		if(geoIpReader == null)
+			logger.warn("ModuleGeoIp.onAppStart [" + appInstance.getContextStr() + "] Build #1 " + ServerListenerGeoIp.PROP_GEOIP_READER + " not set");
+		else
+			logger.info("ModuleGeoIp.onAppStart [" + appInstance.getContextStr() + "] Build #1 countries: " + countries + ", matchAllow: " + matchAllow + ", noMatchAllow: " + noMatchAllow + ", allowedIps: " + allowedIps + ", allowedEncoders: " + allowedEncoders, WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+	}
+
+	public void onConnect(IClient client, RequestFunction function, AMFDataList params)
+	{
+		try
+		{
+			if(debugLog)
+				logger.info(String.format("%s.onConnect [%s] clientIp: %s, flashVer: %s", CLASSNAME, appInstance.getContextStr(), client.getIp(), client.getFlashVer()), category, event);
+			if(SecurityUtils.isValidFlashVersion(client.getFlashVer(), allowedEncoders))
+				return;
+			if(SecurityUtils.isInIPList(client.getIp(), allowedIps))
+				return;
+			if(checkAddress(client.getIp()))
+				return;
+			
+			client.rejectConnection();
+		}
+		catch (Exception e)
+		{
+			logger.error(CLASSNAME + ".onConnect [" + appInstance.getContextStr()  +"] error occured.", e);
+		}
+	}
+
+	public void onHTTPSessionCreate(IHTTPStreamerSession httpSession)
+	{
+		try
+		{
+			if(debugLog)
+				logger.info(String.format("%s.onHTTPSessionCreate [%s] sessionIp: %s", CLASSNAME, appInstance.getContextStr(), httpSession.getIpAddress()), category, event);
+			if(SecurityUtils.isInIPList(httpSession.getIpAddress(), allowedIps))
+				return;
+			if(checkAddress(httpSession.getIpAddress()))
+				return;
+			
+			httpSession.rejectSession();
+			httpSession.setDeleteSession();
+		}
+		catch (Exception e)
+		{
+			logger.error(CLASSNAME + ".onHttpSessionCreate [" + appInstance.getContextStr()  +"] error occured.", e);
+		}
+	}
+
+	public void onRTPSessionCreate(RTPSession rtpSession)
+	{
+		rtpSession.addActionListener(rtspListener);
+	}
+	
+	public boolean checkAddress(String ipAddress)
+	{
+		boolean valid = noMatchAllow;
+		CountryResponse response = null;
+		try
+		{
+			response = geoIpReader.getGeoIpCountry(ipAddress);
+		}
+		catch (GeoIp2Exception e)
+		{
+			logger.warn(String.format("%s.checkAddress [%s]: %s.", CLASSNAME, appInstance.getContextStr(), e.getMessage()), category, event);
+		}
+		
+		if(response != null)
+		{
+			Country country = response.getCountry();
+			if(country != null)
+			{
+				if(debugLog)
+					logger.info(String.format("%s.checkAddress [%s] ipAddress: %s, country: %s", CLASSNAME, appInstance.getContextStr(), ipAddress, country), category, event);
+				String isoCode = country.getIsoCode();
+				for(String countryCode : countries)
+				{
+					if (countryCode.equals("*"))
+					{
+						if(debugLog)
+							logger.info(String.format("%s.checkAddress [%s] ipAddress: %s, isoCode: %s, countryCode: %s is wildcard. returning %b", CLASSNAME, appInstance.getContextStr(), ipAddress, isoCode, countryCode, matchAllow), category, event);
+						valid = matchAllow;
+						break;
+					}
+					if (StringUtils.isEmpty(isoCode))
+					{
+						if(debugLog)
+							logger.info(String.format("%s.checkAddress [%s] ipAddress: %s, isoCode: %s is empty. returning %b", CLASSNAME, appInstance.getContextStr(), ipAddress, isoCode, noMatchAllow), category, event);
+						valid = noMatchAllow;
+						break;
+					}
+
+					if (isoCode.matches(countryCode))
+					{
+						if(debugLog)
+							logger.info(String.format("%s.checkAddress [%s] ipAddress: %s, isoCode: %s, matches countryCode: %s. returning %b", CLASSNAME, appInstance.getContextStr(), ipAddress, isoCode, countryCode, matchAllow), category, event);
+						valid = matchAllow;
+						break;
+					}
+				}
+			}
+		}
+		return valid;
+	}
+
+	private List<String> splitPropertyIntoList(String listStr, boolean isIpList) {
+		ArrayList<String> ret = new ArrayList<String>();
+		if (com.wowza.util.StringUtils.isEmpty(listStr))
+			return ret;
+
+		String[] items = listStr.split("[,|]");
+		for (String item : items) {
+			item = item.trim();
+			if(isIpList)
+			{
+				if (SecurityUtils.isValidIPMatchingString(item))
+					ret.add(item);
+				else
+					logger.warn(String.format("ModuleGeoIp.splitPropertyIntoList[%s]: Invalid IP list string '%s'.", appInstance.getContextStr(), item), category, event);
+			}
+			else
+				ret.add(item);
+		}
+		return ret;
+	}
+}
---
+++ src/com/wowza/wms/plugin/geoip/ServerListenerGeoIp.java
@@ -0,0 +1,248 @@
+/*
+ * This code and all components (c) Copyright 2006 - 2018, Wowza Media Systems, LLC. All rights reserved.
+ * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
+ */
+package com.wowza.wms.plugin.geoip;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.GeoIp2Provider;
+import com.maxmind.geoip2.WebServiceClient;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CountryResponse;
+import com.wowza.util.StringUtils;
+import com.wowza.util.SystemUtils;
+import com.wowza.wms.bootstrap.Bootstrap;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.server.IServer;
+import com.wowza.wms.server.IServerNotify2;
+
+public class ServerListenerGeoIp implements IServerNotify2
+{
+	class MaxmindResponse implements Comparable<MaxmindResponse>
+	{
+		long lastAccess = -1;
+		CountryResponse countryResponse = null;
+
+		public MaxmindResponse(CountryResponse countryResponse, long lastAccess)
+		{
+			this.countryResponse = countryResponse;
+			this.lastAccess = lastAccess;
+		}
+
+		@Override
+		public int compareTo(MaxmindResponse other)
+		{
+			return Long.compare(lastAccess, other.lastAccess);
+		}
+	}
+
+	public static final String PROP_GEOIP_READER = "ServerListenerGeoIp";
+	public static final int CACHE_CAPACITY = 4096;
+
+	private ReadWriteLock lock = new ReentrantReadWriteLock();
+	private Lock readLock = lock.readLock();
+	private Lock writeLock = lock.writeLock();
+	private GeoIp2Provider provider;
+	private int accountId = -1;
+	private String licenseKey = null;
+	private File dbFile = null;
+	private long dbLastModified = -1;
+	private Timer checkTimer = null;
+	private long checkInterval = 1000;
+	private long checkTimeout = checkInterval * 30;
+	private long lastLookup = -1;
+	private boolean shuttingDown = false;
+	private ConcurrentMap<String, MaxmindResponse> cache = new ConcurrentHashMap<String, MaxmindResponse>(CACHE_CAPACITY);
+
+	@Override
+	public void onServerConfigLoaded(IServer server)
+	{
+		accountId = server.getProperties().getPropertyInt("geoIpAccountId", accountId);
+		licenseKey = server.getProperties().getPropertyStr("geoIpLicenseKey", licenseKey);
+		String filePath = server.getProperties().getPropertyStr("geoIpDatabasePath", Bootstrap.getServerHome(Bootstrap.CONFIGHOME) + File.separatorChar + "conf" + File.separatorChar + "GeoIP2-Country.mmdb");
+
+		dbFile = new File(SystemUtils.expandEnvironmentVariables(filePath));
+		if (!dbFile.exists() && dbFile.isFile())
+		{
+			dbFile = null;
+		}
+	}
+
+	@Override
+	public void onServerCreate(IServer server)
+	{
+		server.getProperties().setProperty(PROP_GEOIP_READER, this);
+	}
+
+	@Override
+	public void onServerInit(IServer server)
+	{
+		checkTimer = new Timer();
+		checkTimer.schedule(new TimerTask()
+		{
+
+			@Override
+			public void run()
+			{
+				writeLock.lock();
+				try
+				{
+					if (provider instanceof WebServiceClient)
+					{
+						if (lastLookup + checkTimeout > System.currentTimeMillis())
+						{
+							((WebServiceClient)provider).close();
+							provider = null;
+						}
+					}
+					else if (dbFile.exists() && dbFile.lastModified() > dbLastModified)
+					{
+						if (provider instanceof DatabaseReader)
+						{
+							((DatabaseReader)provider).close();
+							provider = null;
+							cache.clear();
+						}
+						dbLastModified = dbFile.lastModified();
+					}
+
+					if (cache.size() >= CACHE_CAPACITY)
+					{
+						List<MaxmindResponse> cachedResponses = new ArrayList<MaxmindResponse>(cache.values());
+						Collections.sort(cachedResponses);
+						for(MaxmindResponse cachedResponse : cachedResponses)
+						if (cache.size() > CACHE_CAPACITY * 0.75)
+						{
+							cache.values().remove(cachedResponse);
+						}
+					}
+				}
+				catch (IOException e)
+				{
+					WMSLoggerFactory.getLogger(getClass()).error("ServerListenerGeoIp::CheckTimer.run error occured", e);
+				}
+				finally
+				{
+					writeLock.unlock();
+				}
+			}
+		}, 0, checkInterval);
+	}
+
+	@Override
+	public void onServerShutdownStart(IServer server)
+	{
+		writeLock.lock();
+		try
+		{
+			checkTimer.cancel();
+			if (provider != null)
+				((Closeable)provider).close();
+		}
+		catch (IOException e)
+		{
+			// ignore
+		}
+		finally
+		{
+			shuttingDown = true;
+			provider = null;
+			writeLock.unlock();
+		}
+	}
+
+	@Override
+	public void onServerShutdownComplete(IServer server)
+	{
+		// no-op
+	}
+
+	public CountryResponse getGeoIpCountry(String ipAddress) throws GeoIp2Exception
+	{
+		MaxmindResponse response = null;
+		readLock.lock();
+		try
+		{
+			response = cache.get(ipAddress);
+			if (!shuttingDown)
+			{
+				if (response == null)
+				{
+					if (provider == null)
+					{
+						readLock.unlock();
+						writeLock.lock();
+						try
+						{
+							if (provider == null && !shuttingDown)
+							{
+								try
+								{
+									if (accountId != -1 && !StringUtils.isEmpty(licenseKey))
+									{
+										provider = new WebServiceClient.Builder(accountId, licenseKey).build();
+									}
+									else if (dbFile.exists())
+									{
+										provider = new DatabaseReader.Builder(dbFile).build();
+									}
+								}
+								catch (Exception e)
+								{
+									WMSLoggerFactory.getLogger(getClass()).error("ServerListenerGeoIp.checkProvider error occured", e);
+								}
+							}
+							readLock.lock();
+						}
+						finally
+						{
+							writeLock.unlock();
+						}
+					}
+					if (provider != null)
+					{
+
+						InetAddress inetAddress = InetAddress.getByName(ipAddress);
+						CountryResponse countryResponse = provider.country(inetAddress);
+
+						lastLookup = System.currentTimeMillis();
+						response = new MaxmindResponse(countryResponse, lastLookup);
+						cache.putIfAbsent(ipAddress, response);
+					}
+				}
+
+			}
+		}
+		catch (IOException e)
+		{
+			WMSLoggerFactory.getLogger(getClass()).error("ServerListenerGeoIp.getGeoIpCountry error occured", e);
+		}
+		finally
+		{
+			readLock.unlock();
+		}
+
+		if (response != null)
+		{
+			response.lastAccess = System.currentTimeMillis();
+			return response.countryResponse;
+		}
+
+		return null;
+	}
+}
